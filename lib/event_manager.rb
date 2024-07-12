3478540f3d470a983e7a8b6d242d3360f920b339
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-07-12 15:54:28 +0200

    Clean API output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -56,9 +56,18 @@ contents.each do |row|
       roles: %w[legislatorUpperBody legislatorLowerBody]
     )
     legislators = legislators.officials
+
+    # legislator_names = legislators.map do |legislator|
+    #   legislator.name
+    # end
+
+    # or the cleaner one liner
+    legislator_names = legislators.map(&:name)
+
+    legislator_string = legislator_names.join(", ")
   rescue
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
 
-  puts "#{name} #{zipcode} #{legislators}"
+  puts "#{name} #{zipcode} #{legislator_string}"
 end
